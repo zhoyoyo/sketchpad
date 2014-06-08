ed802767b92607283df29944b2b9ca2d0af1a265
zhoyoyo <youyouzhou@dhcp-18-111-11-30.dyn.mit.edu>
2014-06-08 12:41:30 -0400

    1241

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(function() {
 	$('#switch').click(function() {
 		$(this).html("<a href=''>Switch to Collections</a>");
-		$(this)
+		$('.add-link').css({'display':'none'});
+		$('.add-link-expand').css({'display':'none'});
+		$('#main').css({'width':'30%','margin-left':'60%'})
+		$('#storyline').css('display','block');
 	})
 })
